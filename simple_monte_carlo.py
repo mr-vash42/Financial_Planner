704e0ff0bf03d98ae84877515250c2bc7ac69371
vash <mr.vash@gmail.com>
2023-03-23 14:21:26 -0700

    Refactored for multi-period simulation with 2 values.

--- simple_monte_carlo.py
+++ simple_monte_carlo.py
@@ -2,19 +2,25 @@ import unittest
 
 
 class MonteCarloSimulation:
-    def __init__(self, rate_of_return: float, starting_money: float):
-        self.rate_of_return = rate_of_return
+    def __init__(self, rates_of_return: list[float], starting_money: float):
+        self.rates_of_return = rates_of_return
         self.current_money = starting_money
 
     def run(self) -> None:
-        self.current_money = (self.rate_of_return+1)*self.current_money
+        for rate_of_return in self.rates_of_return:
+            self.current_money = (rate_of_return + 1) * self.current_money
 
 
 class MyTestCase(unittest.TestCase):
-    def test_one_period(self):
-        sim = MonteCarloSimulation(rate_of_return=.1, starting_money=1)
+    def test_one_period(self) -> None:
+        sim = MonteCarloSimulation(rates_of_return=[.1], starting_money=1)
         sim.run()
         self.assertEqual(sim.current_money, 1.1)
+
+    def test_two_periods(self) -> None:
+        sim = MonteCarloSimulation(rates_of_return=[.1, .2], starting_money=1)
+        sim.run()
+        self.assertEqual(sim.current_money, 1.32)
 
 
 if __name__ == '__main__':
