b4ccaef587eb7c436a38d9d69fcb6dc2bc5c033c
Timothy Slevin-Vegdahl <mr.vash@gmail.com>
2023-03-23 13:24:19 -0700

    Set up and passed first test for single period with defined return. Added type annotations that will pass mypy but not --strict.

--- simple_monte_carlo.py
+++ simple_monte_carlo.py
@@ -1,9 +1,20 @@
 import unittest
 
 
+class MonteCarloSimulation:
+    def __init__(self, rate_of_return: float, starting_money: float):
+        self.rate_of_return = rate_of_return
+        self.current_money = starting_money
+
+    def run(self) -> None:
+        self.current_money = (self.rate_of_return+1)*self.current_money
+
+
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_one_period(self):
+        sim = MonteCarloSimulation(rate_of_return=.1, starting_money=1)
+        sim.run()
+        self.assertEqual(sim.current_money, 1.1)
 
 
 if __name__ == '__main__':
