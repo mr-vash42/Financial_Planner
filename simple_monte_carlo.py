97e629cbe0bd1de471de74d7fcc8157ba89f1097
vash <mr.vash@gmail.com>
2023-05-13 18:23:59 -0700

    extracted out per period numbers into a dictionary.

--- simple_monte_carlo.py
+++ simple_monte_carlo.py
@@ -15,19 +15,23 @@ class MonteCarloSimulation:
 
 class MyTestCase(unittest.TestCase):
 
-    def assert_simulation_result(self, expected_ending_money: float, rates_of_return: list[float], starting_money: float, expenses: list[float]) -> None:
-        sim = MonteCarloSimulation(rates_of_return, starting_money, expenses)
+    def assert_simulation_result(self, expected_ending_money: float, starting_money: float, per_period_info: dict[str, list[float]]) -> None:
+        sim = MonteCarloSimulation(per_period_info['rates_of_return'], starting_money,
+                                   per_period_info['expenses'])
         sim.run()
         self.assertEqual(sim.current_money, expected_ending_money)
 
     def test_one_period(self) -> None:
-        self.assert_simulation_result(1.1, [.1], 1, [0])
+        per_period_info = {'rates_of_return': [.1], 'expenses': [0.0]}
+        self.assert_simulation_result(1.1, starting_money=1, per_period_info=per_period_info)
 
     def test_two_periods(self) -> None:
-        self.assert_simulation_result(1.32, [.1, .2], 1, [0, 0])
+        per_period_info = {'rates_of_return': [.1, .2], 'expenses': [0.0, 0.0]}
+        self.assert_simulation_result(1.32, starting_money=1, per_period_info=per_period_info)
 
     def test_two_periods_adding_expenses(self) -> None:
-        self.assert_simulation_result(1009949.50, [.01, .01], 1000000, [5000, 5000])
+        per_period_info = {'rates_of_return': [.01, .01], 'expenses': [5000.0, 5000.0]}
+        self.assert_simulation_result(1009949.50, starting_money=1000000, per_period_info=per_period_info)
 
 
 if __name__ == '__main__':
