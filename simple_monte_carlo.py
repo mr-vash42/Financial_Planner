1ac012d5a8c892081b788fad5e50ab32e8d28fc4
vash <mr.vash@gmail.com>
2023-05-14 17:52:34 -0700

    Added income to monte carlo.

--- simple_monte_carlo.py
+++ simple_monte_carlo.py
@@ -1,23 +1,30 @@
 import unittest
+import numpy
 
 
 class MonteCarloSimulation:
-    def __init__(self, rates_of_return: list[float], starting_money: float, expenses: list[float]):
+    def __init__(self, rates_of_return: list[float], starting_money: float, expenses: list[float], incomes: list[float]):
         self.rates_of_return = rates_of_return
         self.current_money = starting_money
         self.expenses = expenses
+        self.incomes = incomes
 
     def run(self) -> None:
-        for rate_of_return, expense in zip(self.rates_of_return, self.expenses):
-            self.current_money = self.current_money - expense
+        for rate_of_return, expense, income in zip(self.rates_of_return, self.expenses, self.incomes):
+            self.current_money = self.current_money - expense + income
             self.current_money = (rate_of_return + 1) * self.current_money
 
 
 class MyTestCase(unittest.TestCase):
 
     def assert_simulation_result(self, expected_ending_money: float, starting_money: float, per_period_info: dict[str, list[float]]) -> None:
+        if 'incomes' in per_period_info:
+            incomes = per_period_info['incomes']
+        else:
+            incomes = list(numpy.zeros(len(per_period_info['rates_of_return'])))
+
         sim = MonteCarloSimulation(per_period_info['rates_of_return'], starting_money,
-                                   per_period_info['expenses'])
+                                   per_period_info['expenses'], incomes)
         sim.run()
         self.assertEqual(sim.current_money, expected_ending_money)
 
@@ -32,6 +39,10 @@ class MyTestCase(unittest.TestCase):
     def test_two_periods_adding_expenses(self) -> None:
         per_period_info = {'rates_of_return': [.01, .01], 'expenses': [5000.0, 5000.0]}
         self.assert_simulation_result(1009949.50, starting_money=1000000, per_period_info=per_period_info)
+
+    def test_two_periods_adding_income(self) -> None:
+        per_period_info = {'rates_of_return': [.01, .01], 'expenses': [5000.0, 5000.0], 'incomes': [1000.0, 1000.0]}
+        self.assert_simulation_result(1011979.6, starting_money=1000000, per_period_info=per_period_info)
 
 
 if __name__ == '__main__':
