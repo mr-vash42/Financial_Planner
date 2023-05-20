56f8ee7a2369cee24f1d4d9aceb6b237f13ce41c
Timothy Slevin-Vegdahl <mr.vash@gmail.com>
2023-05-19 17:27:42 -0700

    Added flat rate tax

--- Taxes.py
+++ Taxes.py
@@ -2,11 +2,12 @@ import unittest
 
 
 class Taxes:
-    def __init__(self, flat_tax):
+    def __init__(self, flat_tax=0, flat_rate_tax=0):
         self.flat_tax = flat_tax
+        self.flat_rate_rate = flat_rate_tax
 
     def get_total_tax(self, gross):
-        return self.flat_tax
+        return self.flat_tax + (gross * self.flat_rate_rate)
 
     def get_income_net_of_taxes(self, gross):
         return gross - self.get_total_tax(gross)
@@ -15,9 +16,14 @@ class Taxes:
 class TestTaxes(unittest.TestCase):
     def test_fixed_cost_tax(self):
         taxes = Taxes(flat_tax=100)
-        gross_income = 1000.
-        self.assertEqual(100., taxes.get_total_tax(gross_income))
-        self.assertEqual(900., taxes.get_income_net_of_taxes(gross_income))
+        self.assertEqual(100., taxes.get_total_tax(1000.))
+        self.assertEqual(900., taxes.get_income_net_of_taxes(1000.))
+
+    def test_flat_rate_tax(self):
+        taxes = Taxes(flat_rate_tax=.15)
+        self.assertEqual(150, taxes.get_total_tax(1000))
+        self.assertEqual(300, taxes.get_total_tax(2000))
+        self.assertEqual(4250, taxes.get_income_net_of_taxes(5000))
 
 
 if __name__ == '__main__':
